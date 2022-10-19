959fde0c919b636e027b92cd0525805118b21a8a
Jose Maria Roman <jose.maria.roman87@gmail.com>
2022-10-19 20:14:20 +0200

    [solar-system] Add noreferrer to external links

--- pages/solar_system.js
+++ pages/solar_system.js
@@ -7,48 +7,48 @@ export default function SolarSystem() {
       <main className={styles.main}>
         <h1 className={styles.title}>Solar System</h1>
         <div>
-          <a href="https://en.wikipedia.org/wiki/Sun" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Sun" target="_blank" rel="noreferrer">
             <div className={styles.sun}>
               <div className={styles.tooltip}>Sun</div>   
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Mercury_(planet)" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Mercury_(planet)" target="_blank" rel="noreferrer">
             <div className={styles.mercury}>
               <div className={styles.tooltip}>Mercury</div>  
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Venus" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Venus" target="_blank" rel="noreferrer">
             <div className={styles.venus}>
               <div className={styles.tooltip}>Venus</div>  
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Earth" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Earth" target="_blank" rel="noreferrer">
             <div className={styles.earth}>
               <div className={styles.tooltip}>Earth</div>  
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Mars" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Mars" target="_blank" rel="noreferrer">
             <div className={styles.mars}>
               <div className={styles.tooltip}>Mars</div>  
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Jupiter" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Jupiter" target="_blank" rel="noreferrer">
             <div className={styles.jupiter}>
               <div className={styles.tooltip}>Jupiter</div>  
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Saturn" target="_blank">        
+          <a href="https://en.wikipedia.org/wiki/Saturn" target="_blank" rel="noreferrer">        
             <div className={styles.saturn}>
               <div className={styles.saturnDisc}></div>
               <div className={styles.tooltip}>Saturn</div>  
             </div>          
           </a>
-          <a href="https://en.wikipedia.org/wiki/Uranus" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Uranus" target="_blank" rel="noreferrer">
             <div className={styles.uranus}>
               <div className={styles.tooltip}>Uranus</div>
             </div>
           </a>
-          <a href="https://en.wikipedia.org/wiki/Neptune" target="_blank">
+          <a href="https://en.wikipedia.org/wiki/Neptune" target="_blank" rel="noreferrer">
             <div className={styles.neptune}>
               <div className={styles.tooltip}>Neptune</div>  
             </div>
