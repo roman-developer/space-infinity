a91f8919afd531e955e993c31e50a95790f6a949
Jose Maria Roman <jose.maria.roman87@gmail.com>
2022-10-19 20:10:16 +0200

    [solar-system] Add links to planet information

--- pages/solar_system.js
+++ pages/solar_system.js
@@ -1,4 +1,5 @@
 import styles from '../styles/SolarSystem.module.css'
+import Link from "next/link";
 
 export default function SolarSystem() {
   return (
@@ -6,34 +7,52 @@ export default function SolarSystem() {
       <main className={styles.main}>
         <h1 className={styles.title}>Solar System</h1>
         <div>
-          <div className={styles.sun}>
-            <div className={styles.tooltip}>Sun</div>   
-          </div>
-          <div className={styles.mercury}>
-            <div className={styles.tooltip}>Mercury</div>  
-          </div>
-          <div className={styles.venus}>
-            <div className={styles.tooltip}>Venus</div>  
-          </div>
-          <div className={styles.earth}>
-            <div className={styles.tooltip}>Earth</div>  
-          </div>
-          <div className={styles.mars}>
-            <div className={styles.tooltip}>Mars</div>  
-          </div>
-          <div className={styles.jupiter}>
-            <div className={styles.tooltip}>Jupiter</div>  
-          </div>
-          <div className={styles.saturn}>
-            <div className={styles.saturnDisc}></div>
-            <div className={styles.tooltip}>Saturn</div>  
-          </div>          
-          <div className={styles.uranus}>
-            <div className={styles.tooltip}>Uranus</div>
-          </div>
-          <div className={styles.neptune}>
-            <div className={styles.tooltip}>Neptune</div>  
-          </div>
+          <a href="https://en.wikipedia.org/wiki/Sun" target="_blank">
+            <div className={styles.sun}>
+              <div className={styles.tooltip}>Sun</div>   
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Mercury_(planet)" target="_blank">
+            <div className={styles.mercury}>
+              <div className={styles.tooltip}>Mercury</div>  
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Venus" target="_blank">
+            <div className={styles.venus}>
+              <div className={styles.tooltip}>Venus</div>  
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Earth" target="_blank">
+            <div className={styles.earth}>
+              <div className={styles.tooltip}>Earth</div>  
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Mars" target="_blank">
+            <div className={styles.mars}>
+              <div className={styles.tooltip}>Mars</div>  
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Jupiter" target="_blank">
+            <div className={styles.jupiter}>
+              <div className={styles.tooltip}>Jupiter</div>  
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Saturn" target="_blank">        
+            <div className={styles.saturn}>
+              <div className={styles.saturnDisc}></div>
+              <div className={styles.tooltip}>Saturn</div>  
+            </div>          
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Uranus" target="_blank">
+            <div className={styles.uranus}>
+              <div className={styles.tooltip}>Uranus</div>
+            </div>
+          </a>
+          <a href="https://en.wikipedia.org/wiki/Neptune" target="_blank">
+            <div className={styles.neptune}>
+              <div className={styles.tooltip}>Neptune</div>  
+            </div>
+          </a>
         </div>
       </main>
     </div>
