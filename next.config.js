6f4e1e96ebfa4bb87de14ebcc4a8f9b82513f8c0
Jose Maria Roman <jose.maria.roman87@gmail.com>
2022-10-23 12:32:58 +0200

    [apod] astronomy picture

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ['mars.jpl.nasa.gov'] }
+  images: { domains: ['mars.jpl.nasa.gov', 'apod.nasa.gov'] }
 }
 
 module.exports = nextConfig
