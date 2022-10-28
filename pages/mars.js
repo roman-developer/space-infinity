7ebab5a2e65ed21a197b920dfd7466e3e4e04a64
Jose Maria Roman <jose.maria.roman87@gmail.com>
2022-10-28 18:03:12 +0200

    [pagination] add pagination to mars photos page (#6)

--- pages/mars.js
+++ pages/mars.js
@@ -1,15 +1,44 @@
 import styles from '../styles/Mars.module.css'
 import Card from '../components/Card';
+import { useState } from 'react';
 
-export default function Mars({photos}) {
+export default function Mars({photos, page, totalPages}) {
+  const [listOfPhotos, setListOfPhotos] = useState(photos);
+  const [currentPage, setCurrentPage] = useState(page);
+
+  const nextPage = async() =>{
+    if(currentPage <= totalPages){
+      setCurrentPage(currentPage + 1);
+      const api_url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${currentPage}&api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`
+      const res = await
+      fetch(api_url);
+      const photos = await res.json();
+      setListOfPhotos(photos);
+    }
+  }
+
+  const prevPage = async() =>{    
+    if(currentPage > 1){
+      setCurrentPage(currentPage - 1);
+      const api_url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${currentPage}&api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`
+      const res = await
+      fetch(api_url);
+      const photos = await res.json();
+      setListOfPhotos(photos);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Mars Rover Photos</h1>
         <div className={styles.grid_photos}>
-          { photos.photos.map((photo) => (
+          { listOfPhotos.photos.map((photo) => (
             <Card key={photo.id} img_src={photo.img_src} sol={photo.sol} camera={photo.camera.full_name} date={photo.earth_date} rover={photo.rover.name}/>
           )) }
+        </div>
+        <div className={styles.pagination}>
+          <button onClick={prevPage}>{`<<`}</button><span className={styles.currentPage}>{currentPage}</span><button onClick={nextPage}>{`>>`}</button>
         </div>
       </main>      
     </div>
@@ -17,10 +46,13 @@ export default function Mars({photos}) {
 }
 
 export async function getStaticProps() {
-  const api_url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`
+  const page = 1
+  const sol = 1000
+  const api_url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&page=${page}&api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`
   const res = await
   fetch(api_url);
   const photos = await res.json();
+  const totalPages = Math.round(photos.photos.length / 25); // (total photos / 25 per page)
 
   if (!photos) {
     return {
@@ -31,6 +63,8 @@ export async function getStaticProps() {
   return {
       props: {
         photos,
+        page,
+        totalPages,
       },
       revalidate: 30,
   };
