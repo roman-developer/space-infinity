4976506a2e718d5deadace86c909438031595485
Jose Maria Roman <jose.maria.roman87@gmail.com>
2025-07-15 12:00:25 +0200

    [solar-system-improvements-geogebra] Add iframe from Geogebra to include Solar System animation

--- pages/solar_system.js
+++ pages/solar_system.js
@@ -1,5 +1,5 @@
 import styles from '../styles/SolarSystem.module.css'
-import Link from "next/link";
+import SolarSystemTable from '../components/SolarSystemTable';
 
 export default function SolarSystem() {
   return (
@@ -7,52 +7,34 @@ export default function SolarSystem() {
       <main className={styles.main}>
         <h1 className={styles.title}>Solar System</h1>
         <div>
-          <a href="https://en.wikipedia.org/wiki/Sun" target="_blank" rel="noreferrer">
-            <div className={styles.sun}>
-              <div className={styles.tooltip}>Sun</div>   
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Mercury_(planet)" target="_blank" rel="noreferrer">
-            <div className={styles.mercury}>
-              <div className={styles.tooltip}>Mercury</div>  
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Venus" target="_blank" rel="noreferrer">
-            <div className={styles.venus}>
-              <div className={styles.tooltip}>Venus</div>  
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Earth" target="_blank" rel="noreferrer">
-            <div className={styles.earth}>
-              <div className={styles.tooltip}>Earth</div>  
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Mars" target="_blank" rel="noreferrer">
-            <div className={styles.mars}>
-              <div className={styles.tooltip}>Mars</div>  
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Jupiter" target="_blank" rel="noreferrer">
-            <div className={styles.jupiter}>
-              <div className={styles.tooltip}>Jupiter</div>  
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Saturn" target="_blank" rel="noreferrer">        
-            <div className={styles.saturn}>
-              <div className={styles.saturnDisc}></div>
-              <div className={styles.tooltip}>Saturn</div>  
-            </div>          
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Uranus" target="_blank" rel="noreferrer">
-            <div className={styles.uranus}>
-              <div className={styles.tooltip}>Uranus</div>
-            </div>
-          </a>
-          <a href="https://en.wikipedia.org/wiki/Neptune" target="_blank" rel="noreferrer">
-            <div className={styles.neptune}>
-              <div className={styles.tooltip}>Neptune</div>  
-            </div>
-          </a>
+          <div>
+            <h2 className={styles.title}>Instructions</h2>
+            <ol className={styles.text}>
+              <li>Click on the Solar System window.</li>
+              <li>Drag and move the cursor within the window.</li>
+              <li>Zoom in or out of the image.</li>
+              <li>Click on full screen to view it in full screen mode.</li>
+              <li>Reload the page if you lose focus</li>
+            </ol>
+            <iframe
+              src="https://www.geogebra.org/material/iframe/id/fcenpwqg/width/1920/height/828/border/888888/sfsb/true/smb/false/stb/false/stbh/false/ai/false/asb/false/sri/false/rc/false/ld/false/sdz/true/ctl/false"
+              width="100%"
+              height="828"
+              scrolling='no'
+              allowFullScreen                         // ✔️ JSX usa camelCase
+              frameBorder="0"                         // ✔️ atributo HTML → camelCase
+              loading="lazy"                          // 🚀 carga diferida
+              title="Solar System - Geogebra"
+              style={{
+                border: '1px solid #e4e4e4',
+                borderRadius: '4px',
+              }}
+            />
+          </div>
+          <div>
+            <h2 className={styles.title}>Information</h2>
+            <SolarSystemTable />
+          </div>
         </div>
       </main>
     </div>
